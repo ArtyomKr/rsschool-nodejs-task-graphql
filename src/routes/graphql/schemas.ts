10d7743c2204dea55ae609ffe13634db9518aa7c
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-07-14 21:36:36 +0300

    feat: create basic graphQL implementation for membersTypes and profiles

--- src/routes/graphql/schemas.ts
+++ src/routes/graphql/schemas.ts
@@ -1,4 +1,17 @@
 import { Type } from '@fastify/type-provider-typebox';
+import {
+  buildSchema,
+  GraphQLBoolean,
+  GraphQLFloat,
+  GraphQLInt,
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+  GraphQLNonNull
+} from 'graphql';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export const gqlResponseSchema = Type.Partial(
   Type.Object({
@@ -18,3 +31,65 @@ export const createGqlResponseSchema = {
     },
   ),
 };
+
+const MemberType = new GraphQLObjectType({
+  name: 'MemberType',
+  fields: () => ({
+    id: {
+      type: GraphQLString
+    },
+    discount: {
+      type: GraphQLFloat
+    },
+    postsLimitPerMonth: {
+      type: GraphQLInt
+    },
+  })
+});
+
+const ProfileType = new GraphQLObjectType({
+  name: 'ProfileType',
+  fields: () => ({
+    id: {
+      type: new GraphQLNonNull(GraphQLString)
+    },
+    isMale: {
+      type: GraphQLBoolean
+    },
+    yearOfBirth: {
+      type: GraphQLInt
+    }
+  })
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQuery',
+  fields: {
+    memberTypes: {
+      type: MemberType,
+      args: { id: { type: GraphQLString } },
+      async resolve(parent, args) {
+        return await prisma.memberType.findUnique({
+          where: {
+            id: args.id,
+          },
+        });
+      }
+    },
+    profile: {
+      type: ProfileType,
+      args: { id: { type: GraphQLString } },
+      async resolve(parent, args) {
+        return await prisma.profile.findUnique({
+          where: {
+            id: args.id,
+          },
+        });
+      }
+    }
+  }
+});
+
+export const schema = new GraphQLSchema({
+  query: RootQuery
+});
