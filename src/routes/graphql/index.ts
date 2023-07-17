20ed541f3b719ee5011a6eb425112aeca69dc1ae
ArtyomKr <tyomik.krasnitsky@gmail.com>
2023-07-17 21:32:38 +0300

    feat: add support for variables, update imports

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,7 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import { createGqlResponseSchema, gqlResponseSchema, schema } from './schemas.js';
+import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
 import { graphql } from 'graphql';
+import { graphqlSchema } from './graphqlSchemas/graphqlSchema.js';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   fastify.route({
@@ -15,7 +16,7 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     async handler(req) {
       const query = req.body.query;
       const variables = req.body.variables;
-      return await graphql({schema, source: query});
+      return await graphql({ schema: graphqlSchema, source: query, variableValues: variables });
     },
   });
 };
